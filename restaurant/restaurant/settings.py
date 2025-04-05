39f51d244ec299133ae934f02bf7fcb2a317401e
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-04-05 21:45:54 +0300

    added the email configurations
    updated the settings file adding the required email details

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -131,6 +131,6 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='ogalomercy8@gmail.com'
-EMAIL_HOST_PASSWORD=''
+EMAIL_HOST_PASSWORD='gvxq tuue jfhy wqrx'
 EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
