43e31f393e0a9bd5b82e1604b96a3d675e9c1668
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-04-06 12:31:10 +0300

    added the postgres database

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "restaurant.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "restaurantly",
+        "USER":"restaurantlyadmin",
+        "Host":"localhost",
+        "PASSWORD":"ogalo1818",
+        "PORT":"5432"
     }
 }
 
